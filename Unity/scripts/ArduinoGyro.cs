e9477279b46ee00853b39747d690f3d546810db6
Juri Berlanda <juriberlanda@hotmail.com>
2015-10-11 19:26:03 +0200

    [Unity] Reworked force application. Steer by yaw implemented using 2nd derivative.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- Unity/scripts/ArduinoGyro.cs
+++ Unity/scripts/ArduinoGyro.cs
@@ -7,15 +7,18 @@ using System.Threading;
 public class ArduinoGyro : ArduinoBase {
 	public const float RAD_TO_DEG = 180f / Mathf.PI;
 	public static Vector3 INVALID_VALUE = Vector3.zero;
-	private float orientationForceScaler = 1f;
+
+	private float JETPACK_FORCE = 10f;
+	private float AERODYNAMICS_FORCE_SCALER = 0.1f;
+	private float YAW_STEER_SCALER = 0.01f;
 
 	public bool _aerodynamics = true;
 	public bool _steerWithYaw = true;
 	public GameObject _controlOrientation;
 	public GameObject _controlPosition;
-	public GameObject _frontFace;
 		
 	private Vector3 _initialOrientation = INVALID_VALUE;
+	private Vector3 _cubeRotation = Vector3.zero;
 
 	protected float GetSectionCoefficient (float orientationDegrees) {
 		return (Mathf.Sin (orientationDegrees / RAD_TO_DEG)) / (Mathf.Cos (orientationDegrees / RAD_TO_DEG));
@@ -53,8 +56,6 @@ public class ArduinoGyro : ArduinoBase {
 
 	void FixedUpdate() {
 		Vector3 yawPitchRoll = Vector3.zero;
-		Vector3 xzVelocity = GetXzVelocity();
-		float yAngleRad = 0f;
 
 		// Apply orientation to the object
 		yawPitchRoll = ParseYawPitchRoll(RequestDataFromArduino('g'));
@@ -63,28 +64,28 @@ public class ArduinoGyro : ArduinoBase {
 
 		if (Input.GetKeyDown(KeyCode.R)) {
 			_initialOrientation = _initialOrientation + yawPitchRoll;
+			_controlPosition.transform.eulerAngles = Vector3.zero;
+			_cubeRotation = Vector3.zero;
 			yawPitchRoll = Vector3.zero;
 		} else
 			_controlOrientation.transform.localEulerAngles = yawPitchRoll;
 
 		// Rotate container to front-face flight direction in X-Z plane as player doesn't move in it.
-		if (!xzVelocity.Equals(Vector3.zero))
-		yAngleRad = Mathf.Sign(xzVelocity.x) * Mathf.Acos(Vector3.Dot(Vector3.forward, xzVelocity.normalized));
-		_controlPosition.transform.eulerAngles = new Vector3(0, yAngleRad * RAD_TO_DEG, 0);
+		_cubeRotation.y += yawPitchRoll.y * YAW_STEER_SCALER;
+		_controlPosition.transform.localEulerAngles = _cubeRotation;
 
 		// Apply forces
+		ApplyJetpackForce(yawPitchRoll);
 		if (_steerWithYaw)
 			ApplySteerWithYaw(yawPitchRoll);
-		ApplyJetpackForce(yawPitchRoll);
 		if (_aerodynamics)
 			ApplyAerodynamicForce(yawPitchRoll);
 	}
 
 	protected void ApplyForceRelativeToBodyDirection(Vector3 force, Vector3 yawPitchRoll) {
-		Vector3 frontFace = Quaternion.AngleAxis(yawPitchRoll.y, Vector3.up) * Vector3.forward;
-		Vector3 absoluteForce = _controlOrientation.transform.InverseTransformVector(force);
-		//absoluteForce.z *= -1;
-		this.GetRigidBody().AddForce(absoluteForce);
+		Vector3 directional_Force = _controlOrientation.transform.InverseTransformDirection(force);
+		directional_Force.z *= -1;
+		this.GetRigidBody().AddForce(directional_Force);
 	}
 
 	private void ApplySteerWithYaw(Vector3 yawPitchRoll) {
@@ -92,7 +93,7 @@ public class ArduinoGyro : ArduinoBase {
 		Vector3 xzVelocity = GetXzVelocity();
 
 		// Fly curve on Yaw
-		force.x -= orientationForceScaler * xzVelocity.magnitude * Mathf.Sin(yawPitchRoll.y / RAD_TO_DEG);
+		force.x += AERODYNAMICS_FORCE_SCALER * xzVelocity.magnitude * Mathf.Sin(yawPitchRoll.y / RAD_TO_DEG);
 		
 		ApplyForceRelativeToBodyDirection(force, yawPitchRoll);
 	}
@@ -100,20 +101,19 @@ public class ArduinoGyro : ArduinoBase {
 	private void ApplyAerodynamicForce(Vector3 yawPitchRoll) {
 		Vector3 force = Vector3.zero;
 
+		// TODO: Get actual wind direction by using velocity
 		// Move forward on Pitch
-		force.z = orientationForceScaler * GetRigidBody().velocity.y * GetSectionCoefficient(yawPitchRoll.x);
+		force.z = AERODYNAMICS_FORCE_SCALER * GetRigidBody().velocity.y * GetSectionCoefficient(yawPitchRoll.x);
 		// Move sideways on Roll
-		force.x = orientationForceScaler * GetRigidBody().velocity.y * GetSectionCoefficient(yawPitchRoll.z);
+		force.x = AERODYNAMICS_FORCE_SCALER * GetRigidBody().velocity.y * GetSectionCoefficient(yawPitchRoll.z);
 
 		ApplyForceRelativeToBodyDirection(force, yawPitchRoll);
 	}
 
 	private void ApplyJetpackForce(Vector3 yawPitchRoll) {
-		Vector3 force = Vector3.zero;
+		Vector3 force = new Vector3(0, 0, -JETPACK_FORCE);
 
 		if (Input.GetKey(KeyCode.Space))
-			force.z = -10f;
-
-		ApplyForceRelativeToBodyDirection(force, yawPitchRoll);
+			ApplyForceRelativeToBodyDirection(force, yawPitchRoll);
 	}
 }
