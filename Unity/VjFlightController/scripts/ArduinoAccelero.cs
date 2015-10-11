ba09b3e89deb788e4ad5bb14eed16d7eca66163a
Juri Berlanda <juriberlanda@hotmail.com>
2015-10-11 19:35:15 +0200

    [Unity] Removed duplicate files.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- Unity/VjFlightController/scripts/ArduinoAccelero.cs
+++
@@ -1,8 +0,0 @@
-public class ArduinoAccelero  {
-	void FixedUpdate() {
-		float accelerationScaler = 0.001f;
-		
-		// Apply acceleration forces
-		//GetRigidBody().AddForce(GetArduinoData('a') * accelerationScaler);
-	}
-}
